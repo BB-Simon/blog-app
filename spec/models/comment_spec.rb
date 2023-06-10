7d482cd96322a72c2b9b1147a426aca9876a9848
BB-Simon <simon.chowdery@gmail.com>
2023-06-10 10:53:24 +0300

    Fix all test cases

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -5,9 +5,12 @@ describe Comment, type: :model do
   let(:test_post) do
     Post.create(author: test_user, title: 'Hello', text: 'This is my first post', comments_counter: 0, likes_counter: 0)
   end
-  let(:test_comment) { Comment.new(post: test_post, author: test_user) }
+  let(:test_comment) { Comment.new(post: test_post, author: test_user, text: 'This mi first comment') }
 
-  before { test_comment.save }
+  before do
+    # test_comment.author_id = test_user.id
+    test_comment.save
+  end
 
   it 'should increment user comment counter by 1' do
     expect(test_post.comments_counter).to eq(1)
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 describe User, type: :model do
-  subject { User.new(name: 'Tom', photo: 'https://unsplash.com/photos', bio: 'Teacher from Mexico.') }
+  subject { User.new(name: 'Tom', photo: 'https://unsplash.com/photos', bio: 'Teacher from Mexico.', post_counter: 0) }
 
   before { subject.save }
   it 'Name should be equal to Tom' do
-    expect(subject).to eq('Tom')
+    expect(subject.name).to eq('Tom')
   end
 
   it 'Name should be present' do
@@ -29,11 +29,11 @@ describe User, type: :model do
   end
 
   it 'user should return top 3 post' do
-    test_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos', bio: 'Teacher from Mexico.')
-    Post.create(author: test_user, title: 'Hello', text: 'This is my first post')
-    Post.create(author: test_user, title: 'Hello', text: 'This is my second post')
-    Post.create(author: test_user, title: 'Hello', text: 'This is my third post')
-    Post.create(author: test_user, title: 'Hello', text: 'This is my fourth post')
+    test_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos', bio: 'Teacher from Mexico.', post_counter: 0)
+    Post.create(author: test_user, title: 'Hello', text: 'This is my first post', comments_counter: 0, likes_counter: 0)
+    Post.create(author: test_user, title: 'Hello', text: 'This is my second post', comments_counter: 0, likes_counter: 0)
+    Post.create(author: test_user, title: 'Hello', text: 'This is my third post', comments_counter: 0, likes_counter: 0)
+    Post.create(author: test_user, title: 'Hello', text: 'This is my fourth post', comments_counter: 0, likes_counter: 0)
 
     expect(test_user.top_3_posts.size).to eq(3)
     expect(test_user.top_3_posts[0].text).to eq('This is my fourth post')
