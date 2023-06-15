c2c5b33e8f27352e810bee07ed2fab0e6a577c09
BB-Simon <simon.chowdery@gmail.com>
2023-06-15 03:06:40 +0300

    Add users initial routes, actions and views

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def show
+    puts params
+  end
+
+  def posts
+    puts params
+  end
+
+  def show_post
+    puts params
+  end
+end
