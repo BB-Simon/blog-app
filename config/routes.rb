50234b4d8e1114e59d75fe5802b2c67485724340
BB-Simon <simon.chowdery@gmail.com>
2023-06-15 03:24:52 +0300

    Add posts routes, actions and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get '/users/:user_id/posts/:id', to: 'posts#show'
+  get '/users/:user_id/posts', to: 'posts#index'
+  get '/users/:id', to: 'users#show'
+  get '/users', to: 'users#index'
+
   # Defines the root path route ("/")
   root 'users#index'
-
-  get '/users/:id', to: 'users#show'
-  get '/users/:id/posts', to: 'users#posts'
-  get '/users/:id/posts/:id', to: 'users#show_post'
 end
