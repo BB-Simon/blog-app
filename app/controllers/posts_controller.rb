f656684536b0967ca058c8379259f96ff9021ec6
BB-Simon <simon.chowdery@gmail.com>
2023-06-17 11:16:03 +0300

    Add create post action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,4 +11,24 @@ class PostsController < ApplicationController
   def new 
     @post = Post.new
   end
+
+  def create
+    @post = Post.new(post_param)
+    @post.comments_counter = 0
+    @post.likes_counter = 0
+    # @post.author_id = current_user.id
+    @post.author_id = params[:user_id]
+
+    if @post.save
+      redirect_to posts_path, notice: 'Post created successfully'
+    else
+      render new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_param
+    params.require(:post).permit(:title, :text)
+  end
 end
