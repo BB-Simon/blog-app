a031f6af32f9163ab19beeb611cdaa96ab38cd11
BB-Simon <simon.chowdery@gmail.com>
2023-06-17 10:03:12 +0300

    Add post new and create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  post '/posts/create', to: 'posts#create'
+  get '/posts/new', to: 'posts#new', as: 'mew_post'
+
   get '/users/:user_id/posts/:id', to: 'posts#show', as: 'post'
   get '/users/:user_id/posts', to: 'posts#index', as: 'posts'
   get '/users/:id', to: 'users#show', as: 'user'
