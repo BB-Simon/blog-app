1c10e2b851e6e19d38bbfe43a627601b1a31df27
BB-Simon <simon.chowdery@gmail.com>
2023-07-01 20:02:36 +0300

    Add authorization resource

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  load_and_authorize_resource
+  
   def index
     @user = User.includes(post: [:author, { comment: [:author] }]).find(params[:user_id])
   end
