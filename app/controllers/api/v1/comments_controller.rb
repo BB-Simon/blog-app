962f07f1de6cc9b30a7b758b68f9453b020164d1
BB-Simon <simon.chowdery@gmail.com>
2023-07-01 20:24:58 +0300

    Add comments api endpoints

---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,35 @@
+class Api::V1::CommentsController < ApplicationController
+  skip_before_action :authenticate_user!
+  protect_from_forgery with: :null_session
+
+  def index
+    post = Post.find(params[:post_id])
+    comments = post.comment
+    render json: comments
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'Post not found' }, status: :not_found
+  rescue StandardError => e
+    render json: { error: e.message }, status: :unprocessable_entity
+  end
+
+  def create
+    post = Post.find(params[:post_id])
+    comment = post.comment.new(comment_params)
+    comment.author = current_user
+    if comment.save
+      render json: comment, status: :created
+    else
+      render json: { errors: comment.errors.full_messages }, status: :unprocessable_entity
+    end
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'Post not found' }, status: :not_found
+  rescue StandardError => e
+    render json: { error: e.message }, status: :unprocessable_entity
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+end
--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,4 +1,7 @@
-class Api::PostsController < ApplicationController
+class Api::V1::PostsController < ApplicationController
+  skip_before_action :authenticate_user!
+  protect_from_forgery with: :null_session
+  
   def index
     user = User.find(params[:user_id])
     posts = user.post
